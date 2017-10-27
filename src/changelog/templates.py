74f6e78a17c2cbf75203d051c9500b1f235b9f27
Ryan McDevitt <mcdevitt.ryan@gmail.com>
2017-10-27 15:38:45 -0400

    add space to initial template

--- src/changelog/templates.py
+++ src/changelog/templates.py
@@ -1,7 +1,8 @@
 BASE = """# CHANGELOG
 
 All notable changes to this project will be documented in this file.
-This project adheres to [Semantic Versioning](http://semver.org/) and [Keep a Changelog](http://keepachangelog.com/)."""
+This project adheres to [Semantic Versioning](http://semver.org/) and [Keep a Changelog](http://keepachangelog.com/).
+"""
 
 UNRELEASED = """
 ## Unreleased
